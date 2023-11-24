93abd083597d6917a2ab7de3bff063d2f44ace37
Nel_ojeda24 <nelojedaarias24@gmail.com>
2023-11-24 09:23:21 +0100

    Fase 3

--- EjemploGit/src/main/java/Principal/Principal.java
+++ EjemploGit/src/main/java/Principal/Principal.java
@@ -5,6 +5,9 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola, Git");
 		System.out.println("Ya se usar GIT");
+		
+		String miVariableString;
+		String miEnteroString;
 	}
 
 }
